1b57766043abe2f0283b2c1517da889992b68f5a
Lauriell <laurie.liegois@gmail.com>
2022-05-20 12:18:31 +0200

    Change

--- Assets/Scripts/SnakeController.cs
+++ Assets/Scripts/SnakeController.cs
@@ -5,18 +5,19 @@ using UnityEngine.InputSystem;
 public class SnakeController : MonoBehaviour
 {
     private bool isAlive = true;
-    private int NextMove;
     private Transform tail;
     [SerializeField] int timeBeforeMove;
 
     private void Start()
     {
         new GameObject("Tail");
+        //  var tail = GameObject("Tail").position;
+        Instantiate(GameObject.Find("TailPart"), transform.parent);
     }
     public void OnMove(InputAction.CallbackContext context)
     {
-        if (!) ;
-        return;
+      /*  if (!(check input)
+        return;*/
 
         while(isAlive == true)
         {
@@ -25,13 +26,24 @@ public class SnakeController : MonoBehaviour
     }
     IEnumerator Move()
     {
-
-        Debug.Log("IE Move");
-        yield return null;
+        if(timeBeforeMove == 0)
+            Debug.Log("IE Move");
+        var emptySpace = GetComponent<Rigidbody>().position;
+        //   emptySpace = Vector2 + currentInput;
+        yield return new WaitForFixedUpdate();
     }
 
     public void EatFruit()
     {
 
+    }
+
+    void Die()
+    {
+        while (isAlive == true)
+        {
+            return;
+        }
+      Instantiate(GameObject.Find("GameOverCanvas"));
     }
 }
